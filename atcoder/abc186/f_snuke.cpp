2cc87a550de196d92b3a386606e083e6dc84dc47
takoyaki65 <takoyaki65@users.noreply.github.com>
2020-12-31 23:43:20 +0900

    abc186 f by snuke

---
+++ atcoder/abc186/f_snuke.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int h, w, n;
+  cin >> h >> w >> n;
+  vector<int> a(w, h), b(h, w);
+  rep(i, n) {
+    int x, y;
+    cin >> x >> y;
+    --x, --y;
+    a[y] = min(a[y], x);
+    b[x] = min(b[x], y);
+  }
+  ll ans = 0;
+  rep(y, b[0]) ans += a[y];
+  rep(x, a[0]) ans += b[x];
+  fenwick_tree<int> t(w);
+  rep(y, b[0]) t.add(y, 1);
+  vector<vector<int>> ends(h + 1);
+  rep(y, b[0]) ends[a[y]].push_back(y);
+  rep(x, a[0]) {
+    for (const int& y : ends[x]) t.add(y, -1);
+    ans -= t.sum(0, b[x]);
+  }
+  cout << ans << endl;
+  return 0;
+}
