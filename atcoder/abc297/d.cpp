d100e136db8a9adad36532e18e554828f20bc3da
takoyaki65 <takoyaki65@users.noreply.github.com>
2023-04-09 23:09:27 +0900

    solve abc297 a~e

---
+++ atcoder/abc297/d.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#define repeat(i, n) for (int i = 0; (i) < (n); ++(i))
+#define repeat_from(i, m, n) for (int i = (m); (i) < (n); ++(i))
+#define bit_stand(n, k) ((n >> k) & 1)
+using namespace std;
+
+template <class T>
+inline int sz(T& x) {
+  return x.size();
+}
+
+template <class T>
+inline bool setmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool setmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <typename T, typename X>
+auto vectors(T a, X x) {
+  return vector<T>(x, a);
+}
+
+template <typename T, typename X, typename Y, typename... Zs>
+auto vectors(T a, X x, Y y, Zs... zs) {
+  auto cont = vectors(a, y, zs...);
+  return vector<decltype(cont)>(x, cont);
+}
+
+int main() {
+  long long a, b;
+  cin >> a >> b;
+  long long ans = 0;
+  // ans: count of subtraction of eratosthenes sieve for a and b
+
+  while (a != b) {
+    if (a < b)
+      swap(a, b);
+    // a > b
+    // cout << a << " " << b << endl;
+    long long count = (a - 1) / b;
+    ans += count;
+    a -= count * b;
+  }
+  cout << ans << endl;
+
+  return 0;
+}
---
+++ atcoder/abc297/e.cpp
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#define repeat(i, n) for (int i = 0; (i) < (n); ++(i))
+#define repeat_from(i, m, n) for (int i = (m); (i) < (n); ++(i))
+#define bit_stand(n, k) ((n >> k) & 1)
+using namespace std;
+
+template <class T>
+inline int sz(T& x) {
+  return x.size();
+}
+
+template <class T>
+inline bool setmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool setmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <typename T, typename X>
+auto vectors(T a, X x) {
+  return vector<T>(x, a);
+}
+
+template <typename T, typename X, typename Y, typename... Zs>
+auto vectors(T a, X x, Y y, Zs... zs) {
+  auto cont = vectors(a, y, zs...);
+  return vector<decltype(cont)>(x, cont);
+}
+
+// N種類のたこ焼き
+// i種類目のたこ焼きの値段はA[i]円
+// 合計で1個以上のたこ焼きを買う。
+// 同じたこ焼きを複数買うこともできる。
+// 支払う金額として有りうるもののうち、安い方からK番目を求める。
+// (1 < N <= 10)
+// (1 <= A[i] <= 200000)
+// (1 <= K <= 1 * 10^9)
+int main() {
+  int n, k;
+  cin >> n >> k;
+  k += 1;
+  vector<long long> a(n);
+  repeat(i, n) cin >> a[i];
+
+  priority_queue<long long, vector<long long>, greater<long long>> que;
+  que.push(0);
+  long long ans = -1;
+  repeat(i, k) {
+    long long ith = -1;
+    while ((ith = que.top()) == ans)
+      que.pop();
+    ans = ith;
+    repeat(j, n) {
+      que.push(ith + a[j]);
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
